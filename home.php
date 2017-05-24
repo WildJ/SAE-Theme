ee391c472b9f4af3fc2e1c73a7ce4cf1cf5b9d3c
WildJ <j.wild@sae.edu>
2017-05-24 17:43:09 +0100

    Custom WP Template

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: Home */ ?> 
+<?php get_header(); ?>
+
+	<div class="row">
+		<div class="col-sm-12">
+			<!-- Your bootstrap custom home page goes here -->
+			
+
+		</div> <!-- /.col -->
+	</div> <!-- /.row -->
+
+<?php get_footer(); ?>
